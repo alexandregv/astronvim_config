6a03721d6b34e806caae477095625f0e35592792
alexandregv <contact@alexandregv.fr>
2024-03-13 22:29:56 +0100

    mappings: mini.{map,trailspace,jump2d}

--- mappings.lua
+++ mappings.lua
@@ -3,6 +3,14 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+
+require('mini.map').setup()
+require('mini.move').setup()
+require('mini.splitjoin').setup()
+require('mini.trailspace').setup()
+MiniTrailspace.trim()
+require('mini.jump2d').setup()
+
 return {
   -- first key is the mode
   n = {
@@ -17,6 +25,20 @@ return {
     --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
     --   desc = "Previous buffer",
     -- },
+
+    -- mini.nvim
+    ["<leader>mo"] = {
+      MiniMap.open,
+      desc = "Open MiniMap",
+    },
+    ["<leader>s"] = {
+      MiniTrailspace.trim,
+      desc = "Open MiniMap",
+    },
+    ["<leader>j"] = {
+      MiniJump2d.start,
+      desc = "Jump to spot",
+    },
 
     -- mappings seen under group name "Buffer"
     ["<leader>bD"] = {
