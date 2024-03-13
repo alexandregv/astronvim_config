3ed653e882f1a10ad8e1074cdc6f9165e8b3a902
alexandregv <contact@alexandregv.fr>
2024-03-13 22:28:47 +0100

    plugins: Add mini.vim, 42-header,nvim, vim-go, vim-terraform, ansible-vim

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,18 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  -- { 'echasnovski/mini.map', version = false, lazy = false },
+  { 'echasnovski/mini.nvim', version = false },
+  -- { 'wfxr/minimap.vim', lazy = false, config = {minimap_auto_start = 1}},
+
+  -- { "andweeb/presence.nvim", lazy = false },
+
+  -- { "Diogo-ss/42-header.nvim", cmd = "Stdheader", keys = "<F1>", opts = {
+  --   default_map = true, -- Default Mapping <F1> in normal mode
+  --   auto_update = true  -- Update header when saving
+  -- }},
+  { "Diogo-ss/42-header.nvim", cmd = "Stdheader", keys = "<F1>", opts = {} },
+
+  { "fatih/vim-go", ft = "go" },
+  { "hashivim/vim-terraform", ft = "terraform" },
+  { "pearofducks/ansible-vim", ft = {"yaml", "jinja2"} },
+
 }
